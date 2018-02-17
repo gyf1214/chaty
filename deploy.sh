f0a7196c93793699b54d4ead35df1f90a6eee30f
gyf1214 <gyf1214@gmail.com>
2018-02-17 19:49:59 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,15 @@
 
 rm -fr deploy/*
 docker-build-go -path=. -cmd=cmd/server
+chmod +x server
 tar zcvf deploy/server.tar.gz conf/ server
 tar zcvf deploy/static.tar.gz static/
 rm server
+
+# deploy to server
+remote=$1
+if [ -n "$remote" ]
+then
+    scp deploy/server.tar.gz $remote:~/chaty
+    ssh $remote tar zxvf chaty/server.tar.gz -C chaty
+fi
